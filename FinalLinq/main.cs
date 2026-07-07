6496343e1cdd5fbd0855035ae8dd516ddaa63df2
Daniel Icaza <danielgicaza@outlook.com>
2026-07-07 12:00:32 -0500

    Add LINQ-based employee filtering and update solution file
    Replaced "Hello, World!" in `main.cs` with a LINQ query to filter
    and process a list of `Empleado` objects based on salary. Added
    a new namespace `LinqClass` for additional functionality. Updated
    the solution file `Linq_Pratices.slnx` to include the new project
    `FinalLinq/FinalLinq.csproj`.

--- FinalLinq/main.cs
+++ FinalLinq/main.cs
@@ -1,10 +1,25 @@
-﻿namespace FinalLinq
+﻿using LinqClass;
+
+namespace FinalLinq
 {
     internal class main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Empleado[] listaEmpleados = new Empleado[]
+            {
+                new Empleado("Juan","Sitemas",2050m),
+                new Empleado("Matias", "Marketing", 1500.25m),
+                new Empleado("Daniel", "TI", 3000.15m),
+                new Empleado("Diana", "Contabilidad", 1500.18m),
+                new Empleado("karla", "Marketing", 2500.25m),
+            };
+
+            var filtro = listaEmpleados.Where(n=>n.Sueldo>=1600m).OrderBy(n => n.Sueldo).Select(n=>n.Nombre);
+            foreach(var i in filtro)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
