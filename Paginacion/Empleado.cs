7a11a204877d47c54a085f2a7b59382d0239a824
Daniel Icaza <danielgicaza@outlook.com>
2026-07-08 07:47:29 -0500

    Creacdion de nuevos archivos como Paginacion con linq

---
+++ Paginacion/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqClass
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Departamento { get; set; }
+        public decimal Sueldo { get; set; }
+        
+        public Empleado(string Nombre, string Departamento, decimal Sueldo)
+        {
+            this.Nombre = Nombre;
+            this.Departamento = Departamento;
+            this.Sueldo = Sueldo;
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Empleado: {Nombre} | Depto: {Departamento} | Sueldo: ${Sueldo}");
+        }
+
+    }
+}
---
+++ Paginacion/main.cs
@@ -0,0 +1,25 @@
+﻿using LinqClass;
+
+namespace Paginacion
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Empleado[] arrayempleados = new Empleado[]
+            {
+                new Empleado("Juan","Sitemas",2050m),
+                new Empleado("Matias", "Marketing", 1500.25m),
+                new Empleado("Daniel", "TI", 3000.15m),
+                new Empleado("Diana", "Contabilidad", 1500.18m),
+                new Empleado("karla", "Marketing", 2500.25m),
+            };
+
+            var lista = arrayempleados.Select(d => d.Departamento).Distinct().OrderBy(d=>d);
+            foreach(var i in lista)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
